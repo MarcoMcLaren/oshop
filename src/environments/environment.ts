65302546a6d34c57038f2ab20bd5b783f47503e8
Marco <mclarenmarco998@gmail.com>
2023-02-23 14:50:45 +0200

    oshop navigation

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAz015LHH0Q_Gl45iHTDam8wBo7HBgmogo",
+        authDomain: "oshop-7219f.firebaseapp.com",
+        projectId: "oshop-7219f",
+        storageBucket: "oshop-7219f.appspot.com",
+        messagingSenderId: "46701025464",
+        appId: "1:46701025464:web:da0bcfae076d16e288fc4d",
+        measurementId: "G-WEGQW9RZV6"  
+    }
+};
